2ac87b2d9a07764e03ea1634d55e5ec57894f9be
Gustavo Sartori <gustavo.sartori@tecnospeed.com.br>
2023-04-27 13:40:40 -0300

    refactor: font-size and minimal content changes in home page

--- app/page.tsx
+++ app/page.tsx
@@ -16,14 +16,14 @@ export default function Home() {
 						<Link
 							key={item.href}
 							href={item.href}
-							className="text-sm duration-500 text-zinc-500 hover:text-zinc-300"
+							className="text-sm md:text-lg duration-500 text-zinc-500 hover:text-zinc-300"
 						>
 							{item.name}
 						</Link>
 					))}
 				</ul>
 			</nav>
-			<div className="hidden w-screen h-px animate-glow md:block animate-fade-left bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
+			<div className="hidden w-screen h-px animate-glow md:block animate-fade-left bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0 mb-3" />
 			<Particles
 				className="absolute inset-0 -z-10 animate-fade-in"
 				quantity={100}
@@ -34,8 +34,8 @@ export default function Home() {
 
 			<div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
 			<div className="my-16 text-center animate-fade-in">
-				<h2 className="text-md px-6 text-zinc-500 md:max-w-2xl md:text-2xl">
-				Hi, I'm Gustavo Almeida, a Full Stack Developer nearly 8 years of experience. I have built two successful companies, visually stunning websites, performant APIs, and smartwatch integrations. I specialize in JS/TS Stack but also have experience with GO and PHP. Currently, I'm looking for new opportunities. Let's chat?
+				<h2 className="text-md px-6 text-zinc-500 md:max-w-2xl md:text-xl xl:text-2xl">
+				Hi, I'm Gustavo Almeida, a Full Stack Developer with nearly 8 years of experience. I have built two successful companies, visually stunning websites, performant APIs, and smartwatch integrations. I specialize in JS/TS Stack but also have experience with GO and PHP. Currently, I'm looking for new opportunities. Let's chat?
 				</h2>
 			</div>
 		</div>
